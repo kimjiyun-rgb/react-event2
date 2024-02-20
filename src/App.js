a0b4da55b614eb0f6526fb93df6121d3cc2750b6
kimjiyun <faren02134@gmail.com>
2024-02-20 19:31:40 +0900

    123456789

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import ConfirmButton from './ConfirmButton';
+import { useState } from 'react';
 
-function App() {
+function MyButton(props)
+{
+  const btns = [1, 2, 3, 4];
+  const handleEvent = (event) =>
+  {
+    console.log(event.target.innerHTML);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {
+          btns.map((btn) => {
+            return (
+              <button onClick={handleEvent}>{btn}</button>
+            )
+          })
+        }
+      </div>
     </div>
+  );
+}
+
+const Toggle = (props) =>
+{
+  const [isToggleOn, setIsToggleOn] = useState(true);
+
+  const handleClick = (e) =>
+  {
+    setIsToggleOn(!isToggleOn);
+  }
+
+  return (<button onClick={(e) => {
+    props.a("내가 했던 것");
+  }}>
+    {isToggleOn ? '켜짐' : '꺼짐'}
+  </button>
+  );
+}
+function App() {
+  const handleClick2 = (action) => {
+    alert(action);
+  }
+  return (
+     <div className="App">
+      <Toggle a={handleClick2} />
+      <MyButton />
+      <ConfirmButton />
+     </div>
   );
 }
 
